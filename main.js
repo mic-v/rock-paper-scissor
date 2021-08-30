cf6d51fc462552039032c0b1d759b182ab0972aa
Michael <michaelvictorino@gmail.com>
2021-08-30 00:22:10 -0400

    Completed barebones of RPS website

--- main.js
+++ main.js
@@ -27,30 +27,74 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    let totalGames = parseInt(document.querySelector("#total").innerHTML);
+    document.querySelector("#total").innerHTML = totalGames + 1; 
     if(playerSelection === "Rock")
     {
         if(computerSelection === "Paper")
-            return "You Lose! Paper beats Rock";
+        {
+            let loss = parseInt(document.querySelector("#loss").innerHTML);
+            document.querySelector("#results").innerHTML = "You Lost! Paper beats Rock";
+            document.querySelector("#loss").innerHTML = loss + 1; 
+        }
         else if(computerSelection === "Scissors")
-            return "You Won! Rock beats Scissors";
+        { 
+            let wins = parseInt(document.querySelector("#wins").innerHTML);
+            document.querySelector("#results").innerHTML = "You Won! Rock beats Scissors";
+            document.querySelector("#wins").innerHTML = wins + 1; 
+        }
         else
-            return "You Tied! Rock equals Rock";
+        {
+            let ties = parseInt(document.querySelector("#ties").innerHTML);
+            document.querySelector("#results").innerHTML = "You Tied! Rock equals Rock";
+            document.querySelector("#ties").innerHTML = ties + 1; 
+        }
+        return false;
 
     } else if (playerSelection === "Paper")
     {
         if(computerSelection === "Scissors")
-            return "You Lose! Scissors beats Paper";
+        {
+            let loss = parseInt(document.querySelector("#loss").innerHTML);
+            document.querySelector("#results").innerHTML = "You Lost! Scissors beats Paper";
+            document.querySelector("#loss").innerHTML = loss + 1; 
+        }
         else if(computerSelection === "Rock")
-            return "You Won! Paper beats Rock";
+        {
+            let wins = parseInt(document.querySelector("#wins").innerHTML);
+            document.querySelector("#results").innerHTML = "You Won! Paper beats Rock";
+            document.querySelector("#wins").innerHTML = wins + 1; 
+        }
         else
-            return "You Tied! Paper equals Paper";
-    } else
+        {
+            let ties = parseInt(document.querySelector("#ties").innerHTML);
+            document.querySelector("#results").innerHTML = "You Tied! Paper equals Paper";
+            document.querySelector("#ties").innerHTML = ties + 1; 
+        }
+        return false;
+    } 
+    else
     {
         if(computerSelection === "Rock")
-            return "You Lose! Rock beats Scissors";
+        {
+            let loss = parseInt(document.querySelector("#loss").innerHTML);
+             document.querySelector("#results").innerHTML = "You Lost! Rock beats Scissors";
+             document.querySelector("#loss").innerHTML = loss + 1; 
+        }
         else if(computerSelection === "Paper")
-            return "You Won! Scissors beats Paper";
+        {
+            let wins = parseInt(document.querySelector("#wins").innerHTML);
+            document.querySelector("#results").innerHTML = "You Won! Scissors beats Paper";
+            document.querySelector("#wins").innerHTML = wins + 1; 
+        }
         else
-            return "You Tied! Scissors equals Scissors";
+        {
+            let ties = parseInt(document.querySelector("#ties").innerHTML);
+            document.querySelector("#results").innerHTML = "You Tied! Scissors equals Scissors";
+            document.querySelector("#ties").innerHTML = ties + 1; 
+        }
+    
+        return false;
     }
+    return false;
 }
